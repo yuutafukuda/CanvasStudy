50f0f8067fee18453d631c4d86fef364162b2fd2
yuutafukuda <viyutafukuda@gmail.com>
2022-07-26 13:01:10 -0300

    Added animation on circle

--- js/canvas.js
+++ js/canvas.js
@@ -32,3 +32,32 @@ for (var i = 0; i < 3; i++) {
     c.strokeStyle = 'rgba(' + (x * 255) + ',' + (y * 255) + ',' + ((x + y) * 255/2) + ', .5)';
     c.stroke(); 
 }
+
+//Animation
+var x = Math.random() * innerWidth;
+var y = Math.random() * innerHeight;
+var dx = 5;
+var dy = 5;
+let radius = 30;
+
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, innerWidth, innerHeight);
+
+    c.beginPath();
+    c.arc(x, y, radius, 0, Math.PI * 2, true);
+    c.strokeStyle = 'red';
+    c.stroke();
+    
+    if (x + radius > innerWidth || x - radius < 0) {
+        dx = -dx;
+    }
+    
+    if ( y + radius > innerHeight || y - radius < 0){
+        dy = -dy;
+    }
+    x += dx;
+    y += dy;
+}
+
+animate();
