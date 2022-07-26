38551b1be4aeefa63bde1e974cad4bb752364236
yuutafukuda <viyutafukuda@gmail.com>
2022-07-25 23:16:34 -0300

    Added circles to canvas

--- js/canvas.js
+++ js/canvas.js
@@ -6,7 +6,7 @@ canvas.height = window.innerHeight;
 const c = canvas.getContext('2d');
 
 // Rect
-c.fillStyle = 'blue';
+c.fillStyle = 'rgba(0, 255, 0, .8)';
 c.fillRect(10, 10, 100, 100);
 
 // Line
@@ -20,5 +20,16 @@ c.stroke();
 // Arc or Circle
 c.beginPath();
 c.arc(300, 300, 30, 0, Math.PI * 2, true);
-c.strokeStyle = 'rgba(255, 0, 0, .5)';
+c.strokeStyle = 'blue';
 c.stroke();
+
+for (var i = 0; i < 3; i++) {
+    var x = Math.random();
+    var y = Math.random();
+
+    c.beginPath();
+    c.arc(x * window.innerWidth/2, y * window.innerHeight/2, 30, 0, Math.PI * 2, true);
+    c.strokeStyle = 'rgba(' + (x * 255) + ',' + (y * 255) + ',' + ((x + y) * 255/2) + ', .5)';
+    c.stroke(); 
+    
+}
